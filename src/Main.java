2c171bb655782ebec3995caf985738d864e546e6
Tharusha Thilakshan <tharushajayasekara04@gmail.com>
2024-03-12 16:24:12 +0530

    MethodOverloading

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Pattern d1=new Pattern();
+
+        d1.display();
+        System.out.println("\n");
+
+        d1.display('#');
+
+        }
+    }
---
+++ src/Pattern.java
@@ -0,0 +1,13 @@
+public class Pattern {
+    public void display(){
+        for (int i=0;i<10;i++);{
+            System.out.println("*");
+        }
+    }
+    public void display(char symbol){
+        for (int i=0;i<10;i++){
+            System.out.println(symbol);
+        }
+
+    }
+}
